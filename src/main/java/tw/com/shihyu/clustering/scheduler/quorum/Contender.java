95590fa01734e2178fb5217a7cb652eefdcf9a29
Matt S.Y. Ho <methodho@gmail.com>
2016-06-13 16:43:46 +0800

    add @EqualsAndHashCode

--- src/main/java/tw/com/shihyu/clustering/scheduler/quorum/Contender.java
+++ src/main/java/tw/com/shihyu/clustering/scheduler/quorum/Contender.java
@@ -1,6 +1,7 @@
 package tw.com.shihyu.clustering.scheduler.quorum;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +16,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class Contender {
 
   private String id;
